2f0ed091f5219d9ad77fe7bd61741f4a3fcb4a91
David Moore <hey@iamdavidmoore.com>
2018-07-06 22:16:55 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -36,7 +36,9 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = [:release_date]
+    m.save
   end
+
 end
 
 def can_get_the_first_item_in_the_database
